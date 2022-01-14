8f904f47881a8ca34775f96d1c7f26fd7d695b7a
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-14 11:21:30 +0100

    dates

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 // Premiere ligne
+$date = getdate();
+echo $date['weekday'];
+echo "<br><br>";
 
+// Deuxieme ligne
+echo date ("Y.m.d", mktime(null, null,null,12,10,2018));
+echo "<br><br>";
 
-
-
-
+// Troisieme ligne
+echo date("h:i:s", mktime(11,35,07));
 
